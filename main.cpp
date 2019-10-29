15a76b1b8f2ebe37fcde042f1f983c792cdbcd0a
Rustam <rustam.guseynov@thomsonreuters.com>
2019-10-29 14:13:25 +0300

    First commit

--- main.cpp
+++ main.cpp
@@ -5,100 +5,148 @@ using namespace std;
 
 //#define TEST
 
-bool starts_with(const string& who, const string& what) {
-    if (who.length() <= what.length()) return false;
+bool starts_with(const string &who, const string &what)
+{
+    if (who.length() <= what.length())
+        return false;
     return who.find(what) == 0;
 }
 
-int getFinalInt(const string& what, int start_pos) {
-    string arg = what.substr(start_pos);
+int get_ending_integer(const string &what)
+{
+    int start_space_pos = what.find_first_of(' ');
+    string arg = what.substr(start_space_pos + 1);
     return atoi(arg.c_str());
 }
 
-vector<int> worried_queue(const vector<string>& actions) {
-    int worry_count = 0;
-    vector<bool> worried_people;
-    vector<int> res;
-    for (auto command: actions) {
-        // cout << command << endl;
-        if (command == "WORRY_COUNT") {
-            res.push_back(worry_count);
-        } else if (starts_with(command, "COME")) {
-            int came = getFinalInt(command, 5);
-            if (came > 0) {
-                worried_people.insert(worried_people.end(), came, false);
-            } else if (came < 0) {
-                worried_people.resize(worried_people.size() + came);
-                worry_count = 0;
-                for (auto person: worried_people) {
-                    if (person) worry_count++;
+int get_middle_integer(const string &what)
+{
+    int start_space_pos = what.find_first_of(' ');
+    int end_space_pos = what.find_last_of(' ');
+    string arg = what.substr(start_space_pos + 1, end_space_pos - start_space_pos);
+    return atoi(arg.c_str());
+}
+
+string get_ending_string(const string &what)
+{
+    int end_space_pos = what.find_last_of(' ');
+    string arg = what.substr(end_space_pos + 1);
+    return arg;
+}
+
+auto todo(const vector<string> &actions)
+{
+    vector<string> res;
+    const vector<int> count_days{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int current_month = 0;
+    vector<vector<string>> tasks;
+    tasks.assign(count_days[current_month], vector<string>());
+
+    for (auto command : actions)
+    {
+        if (starts_with(command, "DUMP"))
+        {
+            auto date = get_ending_integer(command);
+            // cout << "DUMP " << date << endl;
+            string str;
+            str.append(tasks[date].size() + " ");
+            for (auto record : tasks[date])
+            {
+                str.append(record + " ");
+            }
+            res.push_back(str);
+        }
+        else if (command == "NEXT")
+        {
+            auto last_month = current_month;
+            current_month = (current_month + 1) % 12;
+            // cout << "NEXT " << current_month << endl;
+
+            if (count_days[current_month] < count_days[last_month])
+            {
+                auto &new_last_day_of_month = tasks[count_days[current_month] - 1];
+
+                for (auto i = count_days[last_month] - 1; i > count_days[current_month]; i++)
+                {
+                    auto last_day_tasks = tasks.back();
+                    new_last_day_of_month.insert(new_last_day_of_month.end(), last_day_tasks.begin(), last_day_tasks.end());
+                    tasks.pop_back();
                 }
             }
-        } else if (starts_with(command, "WORRY")) {
-            int worried_pos = getFinalInt(command, 6);
-            auto was_worried = worried_people[worried_pos];
-            if (!was_worried) {
-                worried_people[worried_pos] = true;
-                worry_count++;
-            }
-        } else if (starts_with(command, "QUIET")) {
-            int quiet_pos = getFinalInt(command, 6);
-            auto was_worried = worried_people[quiet_pos];
-            if (was_worried) {
-                worried_people[quiet_pos] = false;
-                worry_count--;
+            else if (count_days[current_month] > count_days[last_month])
+            {
+                tasks.insert(tasks.end(), vector<string>());
             }
         }
-        // cout << "queue length now " << worried_people.size() << " and number of worried is " << worry_count << endl;
+        else if (starts_with(command, "ADD"))
+        {
+            auto date = get_middle_integer(command);
+            auto name = get_ending_string(command);
+
+            // cout << "ADD " << date << " " << name << endl;
+
+            tasks[date].push_back(name);
+        }
     }
     return res;
 }
 
-
 #ifdef TEST
-    template<typename T> bool vectors_equal(const vector<T>& v1, const vector<T>& v2) {
-        if (v1.size() != v2.size()) return false;
-        for (int i = 0; i < v1.size(); i++)  {
-            if (v1[i] != v2[i]) return false;
-        }
-        return true;
+template <typename T>
+bool vectors_equal(const vector<T> &v1, const vector<T> &v2)
+{
+    if (v1.size() != v2.size())
+        return false;
+    for (int i = 0; i < v1.size(); i++)
+    {
+        if (v1[i] != v2[i])
+            return false;
     }
+    return true;
+}
 
-    bool test_1() {
-        vector<string> commands{"COME 5", "WORRY 1", "WORRY 4", "COME -2", "WORRY_COUNT", "COME 3", "WORRY 3", "WORRY_COUNT"};
-        auto res = worried_queue(commands);
-        vector<int> expected{1, 2};
-        for (auto item: res) {
-            cout << item << endl;
-        }
-        return vectors_equal(res, expected);
+bool test_1()
+{
+    vector<string> commands{"COME 5", "WORRY 1", "WORRY 4", "COME -2", "WORRY_COUNT", "COME 3", "WORRY 3", "WORRY_COUNT"};
+    auto res = worried_queue(commands);
+    vector<int> expected{1, 2};
+    for (auto item : res)
+    {
+        cout << item << endl;
     }
+    return vectors_equal(res, expected);
+}
 #else
-    int run() {
-        int num;
-        cin >> num;
-        cin >> ws;
-        vector<string> commands(num);
-        for (auto& command: commands) {
-            getline (cin, command);
-        }
-        auto result = worried_queue(commands);
-        for (auto item: result) {
-            cout << item << endl;
-        }
-        return 0;
+int run()
+{
+    int num;
+    cin >> num;
+    cin >> ws;
+    vector<string> commands(num);
+    for (auto &command : commands)
+    {
+        getline(cin, command);
     }
+    auto result = todo(commands);
+    for (auto item : result)
+    {
+        cout << item << endl;
+    }
+    return 0;
+}
 #endif
 
-int main() {
-    #ifdef TEST
-        if (!test_1()) {
-            cout << "FAIL!" << endl;
-            return -1;
-        }
-        cout << "OK!" << endl;
-        return 0;
-    #else 
-        return run();
-    #endif
+int main()
+{
+#ifdef TEST
+    if (!test_1())
+    {
+        cout << "FAIL!" << endl;
+        return -1;
+    }
+    cout << "OK!" << endl;
+    return 0;
+#else
+    return run();
+#endif
 }
