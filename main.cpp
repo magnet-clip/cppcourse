08f1350ff3641b99e1262ecd0f90e3510f819c40
Rustam <rustam.guseynov@refinitiv.com>
2019-11-20 23:27:43 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,52 +1,28 @@
-#include <algorithm>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
-#include <map>
-#include <set>
-#include <sstream>
-#include <tuple>
-#include <vector>
 
 using namespace std;
 
-//#define TEST
-
-#ifdef TEST
-
-bool test_1() { return true; }
-
-bool test_2() { return true; }
-
-#else
-
-int run() {
-  ifstream input("input.txt");
-  ofstream output("output.txt");
-  string line;
-  while (getline(input, line)) {
-    output << line << endl;
-  }
-  return 0;
-}
-#endif
-
 int main() {
-#ifdef TEST
-  if (!test_1()) {
-    cout << "TEST 1 FAIL!" << endl;
-    return -1;
-  } else {
-    cout << "Test 1 OK!" << endl;
+  ifstream input("input.txt");
+
+  int value;
+  int rows, cols;
+
+  input >> rows >> cols;
+
+  for (auto r = 0; r < rows; r++) {
+    for (auto c = 0; c < cols - 1; c++) {
+      input >> value;
+      input.ignore(1);
+      cout << setw(10) << right << value << " ";
+    }
+    input >> value;
+    input.ignore(1);
+    cout << setw(10) << right << value;
+    if (r < rows - 1) {
+      cout << endl;
+    }
   }
-  if (!test_2()) {
-    cout << "TEST 2 FAIL!" << endl;
-    return -1;
-  } else {
-    cout << "Test 2 OK!" << endl;
-  }
-  cout << "OK!" << endl;
-  return 0;
-#else
-  return run();
-#endif
 }
